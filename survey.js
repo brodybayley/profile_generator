7fc6850ef061a889530a0aafc1d313dbc4999c10
Brody Bayley <brodybayley@gmail.com>
2021-02-10 03:22:53 +0000

    adding questions

--- survey.js
+++ survey.js
@@ -5,13 +5,26 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
 
-  rl.close();
-});
-rl.question("What's an activity you like doing? ", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+  rl.question("What's an activity you like doing? ", (activity) => {
 
-  rl.close();
+    rl.question("What do you listen to while doing that? ", (listen) => {
+
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (meal) => {
+
+        rl.question("What's your favourite thing to eat for that meal? ", (dish) => {
+
+          rl.question("Which sport is your absolute favourite? ", (sport) => {
+
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (superpower) => {
+              console.log(`${name} loves ${activity} while listening to ${listen}. But omg their favourite meal is ${meal} and they love eating ${dish}. They're a huge fan of ${sport}, but their real superpower is ${superpower}.`);
+
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
